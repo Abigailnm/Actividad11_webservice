a1771452d22d1d28a78a6e66cc73982ac8f32299
Abigail Nery Montes <1717110627@utectulancingo.edu.mx>
2019-03-27 19:45:07 -0600

    web service terminada

---
+++ config.py
@@ -0,0 +1,19 @@
+import web
+
+db_host = 'localhost'
+db_name = 'ferreteria_nema'
+db_user = 'ferret'
+db_pw = 'ferret.2019'
+"""
+db_host = 'gk90usy5ik2otcvi.cbetxkdyhwsb.us-east-1.rds.amazonaws.com'
+db_name = 'j9gk65oe7j5df2be'
+db_user = 'wneatin9hw8g62bh'
+db_pw = 'zgznjrvcv6n3nq4d'
+"""
+db = web.database(
+    dbn='mysql',
+    host=db_host,
+    db=db_name,
+    user=db_user,
+    pw=db_pw
+    )
